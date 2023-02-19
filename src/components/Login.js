53a2aaae9e9caffa04940b3c8178f85edb4ee162
Potla Vivek <potlavivek@gmail.com>
2023-02-19 15:30:37 +0530

    firebase setup branch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import Alert from 'react-bootstrap/Alert';
 import {Link} from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import {  signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../firebase.js'
+import { GoAlert } from "react-icons/go";
 function Login() {
+
+  const [err, setErr] = useState(false)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onFormSubmit = (userCredObj) => {
+      let email= userCredObj.email;
+      let password=userCredObj.password
+      console.log(userCredObj)
+
+      signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in successfully
+        //console.log("User logged in " , userCredential)
+        const user = userCredential.user;
+        console.log("Logged in User is " , user)
+        // ...
+
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+       // const errorMessage = error.message;
+       // console.log(errorCode)
+          if(errorCode==='auth/wrong-password')
+          setErr("Wrong Password.! Try Again..!")
+          else if(errorCode==='auth/user-not-found')
+          setErr("User Not Found...!")
+          else if(errorCode==='auth/too-many-requests')
+          setErr('Too many requests..! Try after some time')
+          else setErr(errorCode)
+      });
+
+  }
   return (
+
     <div className='loginbody pt-5 pb-5'>
       {/* To be removed after development */}
       <span><Link to='/home' >GO to Home</Link></span>
@@ -13,17 +50,17 @@ function Login() {
 
     <h3 className="text-center">Login</h3>
 
-    <form className="signupform  m-5 mx-auto border border-2 border-secondary p-5 " >
+    <form className="signupform  m-5 mx-auto border border-2 border-secondary p-5 " onSubmit={handleSubmit(onFormSubmit)} >
 
    
     {/* Username and Mobile number */}
       <div className="row mb-5 gap-5">
         <div className="col-sm-12 form-floating">
-          <input type="text" className="form-control border border-2 border-secondary" placeholder="Username" />
-          <label  className="form-label ms-3">Username</label>
+          <input type="text" className="form-control border border-2 border-secondary" placeholder="email" {...register("email",{required:true})}/>
+          <label  className="form-label ms-3">Email</label>
         </div>
         <div className="col-sm-12 form-floating">
-          <input type="password" className="form-control border border-2 border-secondary" placeholder="no"/>
+          <input type="password" className="form-control border border-2 border-secondary" placeholder="password" {...register("password",{required:true})}/>
           <label  className="form-label ms-3">Password</label>
         </div>
       </div>
@@ -33,10 +70,15 @@ function Login() {
       </div>
 
       {/* Submit Button */}
-      <div className="text-center mt-5 ms-4">
+      <div className="text-center mt-5 ms-4 mb-4">
         <button type="submit" className="btn btn-lg btn-success  ">Login</button>
       </div>
-
+      {err && 
+         
+          <Alert  variant='danger' className='p-1 ps-3  m-0'>
+            <GoAlert className='mb-1' /> {err}
+        </Alert>
+        }
     </form>
     
 </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import {  createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import Alert from 'react-bootstrap/Alert';
+import {auth,storage ,db} from '../firebase.js'
+import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { AiOutlinePicture,AiOutlineMail } from "react-icons/ai";
+import { doc, setDoc } from "firebase/firestore"; 
 function SignUp() {
+  let navigate= useNavigate();
+  const [err,setErr] = useState(false)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  
+  const onFormSubmit = async (userCredObj) => {
+    const displayName =userCredObj.username
+    const email = userCredObj.email
+    const password = userCredObj.password
+    const file = userCredObj.file
+    console.log(userCredObj);
+    try{
+          let res = await createUserWithEmailAndPassword(auth, userCredObj.email, userCredObj.password)
+          //userCredentialObject
+
+                  const storageRef =  ref(storage, displayName`);
+                  const uploadTask =await uploadBytesResumable(storageRef, userCredObj.file);
+
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.then(
+              
+              () => {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                     try{ 
+                     await updateProfile(res.user,{
+                        displayName,
+                        photoURL :downloadURL,
+
+                      })
+                      await setDoc(doc(db,"users",res.user.uid),{
+                        uid: res.user.uid,
+                        displayName,
+                        email,
+                        photoURL: downloadURL,
+                      })  
+                    }
+                    catch(err)
+                    {
+                      console.log("Error :" , err)
+                      setErr(err.code)
+                    }
+                      
+                });
+              });
+                
+
+
+              
+            //      //Object.keys(res.user)
+            //         //  ['providerId', 'proactiveRefresh', 'reloadUserInfo', 'reloadListener', 
+            //         // 'uid', 'auth', 'stsTokenManager', 'accessToken', 'displayName', 'email', 'emailVerified', 
+            //         // 'phoneNumber', 'photoURL', 'isAnonymous', 'tenantId', 'providerData', 'metadata']
+          setErr(false);
+          //navigates to login page after signing up
+          
+          
+        }
+        catch(err)
+        {
+          console.log("error " ,err.code)
+          if(err.code==='auth/email-already-in-use')
+          setErr("Email already in use..! Try with another one")
+        }
+
+    
+  }
+ 
+
+
   return (
     <div className='signupbody pt-5 pb-5'>
       <div className="text-center lead">
@@ -10,42 +90,53 @@ function SignUp() {
       </div>
 
       <h3 className="text-center">Sign Up</h3>
+     
+      <form className="signupform  m-5 mx-auto border border-2 border-secondary p-5 " onSubmit={handleSubmit(onFormSubmit)} >
 
-      <form className="signupform  m-5 mx-auto border border-2 border-secondary p-5 " >
-
-      {/* Names */}
+      {/* UserName */}
         <div className="row mb-4">
-          <div className="col-sm-6 form-floating ">
-            <input type="text" className="form-control border border-2 border-secondary" id="fname" placeholder="fname"/>
-            <label  className="form-label ms-3">First Name</label>
+          <div className=" form-floating ">
+            <input type="text" className="form-control border border-2 border-secondary" id="username" placeholder="username" {...register("username",{required:true})}/>
+            <label  className="form-label ms-3">UserName</label>
           </div>
-          <div className="col-sm-6 form-floating">
-            <input type="text" className="input form-control border border-2 border-secondary" id="lname" placeholder="fname"/>
-            <label className="form-label ms-3">Last Name</label>
+          {errors.username?.type === 'required' && <p role="alert" className='text-danger'>* Username is required</p>}
+         </div>
+      {/* Pw*/}
+        <div className="row mb-4">
+          <div className=" form-floating">
+            <input type="text" className="form-control border border-2 border-secondary" placeholder="password"  {...register("password", { required: "* Password is required", minLength: { value: 6, message: "* Password must be atleast 6 characters" }, maxLength: { value: 12, message: "* Password cannot exceed more than 12 characters" }})}/>
+            <label  className="form-label ms-3">Password</label>
+            {errors.password  && <p role="alert" className='text-danger p-0 m-0'>{errors.password.message}</p>}
           </div>
+          
         </div>
-      {/* Username and Mobile number */}
-        <div className="row mb-5">
-          <div className="col-sm-6 form-floating">
-            <input type="text" className="form-control border border-2 border-secondary" placeholder="Username" />
-            <label  className="form-label ms-3">@Username</label>
-          </div>
-          <div className="col-sm-6 form-floating">
-            <input type="number" className="form-control border border-2 border-secondary" placeholder="no"/>
-            <label  className="form-label ms-3">Mobile No.</label>
-          </div>
+        <div className='row mb-4'>
+          <div className="form-floating">
+              <input type="number" className="form-control border border-2 border-secondary" placeholder="Mobile No.(+91)" {...register("phoneNumber",{required:true})}/>
+              <label  className="form-label ms-3">Mobile No.</label>
+
+            </div>
+            {errors.phoneNumber?.type === 'required' && <p role="alert" className='text-danger'>* Mobile number is required</p>}
         </div>
 
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-2">
-            <label  className="form-label">
-              <img className="img1" src="https://cdn.icon-icons.com/icons2/2768/PNG/512/email_icon_176616.png" alt=""/>
-            </label>
+          <AiOutlineMail size='lg' className='mx-auto'/>
           </div>
           <div className="col form-floating">
-            <input type="email" className="form-control border border-2 border-secondary  p-2" placeholder="email" id="email"/>
+            <input type="email" className="form-control border border-2 border-secondary " placeholder="email" id="email" {...register("email",{required:true})}/>
             <label  className="form-label ms-3">Email</label>
           </div>
+          {errors.email?.type === 'required' && <p role="alert" className='text-danger'>* Email ID is required</p>}
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <AiOutlinePicture size='lg' className='mx-auto'/>
+          </div>
+          <div className="col ">
+            <input type="file" className="form-control border border-2 border-secondary  p-2" accept="image/png, image/jpeg,image/jpg" placeholder="Profile Photo" id="Image" {...register("file",{required:true})}/>
+          </div>
+          {errors.file?.type === 'required' && <p role="alert" className='text-danger'>* Upload a photo</p>}
         </div>
 
         <div className="mt-5 text-end">
@@ -53,9 +144,14 @@ function SignUp() {
         </div>
 
         {/* Submit Button */}
-        <div className="text-center mt-5 ms-4">
+        <div className="text-center mt-5 ms-4 mb-3">
           <button type="submit" className="btn btn-lg btn-success  ">Create An Account</button>
         </div>
+        {err && 
+          <Alert  variant='danger'>
+          {err}
+        </Alert>
+        }
 
       </form>
 </div>
