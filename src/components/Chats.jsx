754378dedcb1cb5b9e01d86a0b8297900253ecf4
Neeha Bathula <neehabathula@gmail.com>
2023-03-08 23:46:13 +0530

    search and chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,21 +3,21 @@ import './Chats.css'
 function chats() {
   return (
     <div className=''>
-    <div className='chats ps-2'>
+    <div className='chats ps-2 border'>
           <img src='https://i.pinimg.com/236x/01/4b/ba/014bba6c5196101bf43042c820b92db0.jpg' className='searchimg' alt=""/>
           <div className='mt-3'>
           <span className='username'>Neeha</span>
           <p className='msg'>Hello</p>
           </div>
     </div>
-      <div className='chats ps-2'>
+      <div className='chats ps-2 border-bottom'>
         <img src='https://i.pinimg.com/236x/01/4b/ba/014bba6c5196101bf43042c820b92db0.jpg' className='searchimg' alt=""/>
         <div className='mt-3'>
         <span className='username'>Neeha</span>
         <p className='msg'>Hello</p>
         </div>
       </div>
-      <div className='chats ps-2'>
+      <div className='chats ps-2 border-bottom'>
         <img src='https://i.pinimg.com/236x/01/4b/ba/014bba6c5196101bf43042c820b92db0.jpg' className='searchimg' alt=""/>
         <div className='mt-3'>
         <span className='username'>Neeha</span>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { BsSearch } from "react-icons/bs";
+import { collection, query, where, getDocs, getDoc, setDoc,doc, updateDoc, serverTimestamp } from "firebase/firestore";
+import {db} from '../firebase'
+import {AuthContext} from '../context/AuthContext'
 import './Search.css'
-function search() {
+import './Chats.css'
+function Search() {
+  const [username,setUsername]=useState("")
+  const [user,setUser]=useState(null)
+  const [err,setErr]=useState(false)
+  const {currentUser} =useContext(AuthContext) 
+  const handleSelect =async()=>{
+    //check if the chats collection for two users exist or not and create new one if no
+    console.log(currentUser)
+    console.log(user)
+    const combinedId=
+    currentUser.uid > user.uid
+    ? currentUser.uid + user.uid
+    : user.uid + currentUser.uid
+    try{
+      const res=await getDoc(doc(db,"chats",combinedId))
+      if(!res.exists()){
+        //create chat in chats collection
+        await setDoc(doc(db,"chats",combinedId),{messages:[]})
+        //create user chats in firestore
+        await updateDoc(doc(db,"userchats",currentUser.uid),{
+          [combinedId.uid+"userInfo"]:{
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedId+"date"]:serverTimestamp()
+        })
+        await updateDoc(doc(db,"userchats",user.uid),{
+          [combinedId.uid+"userInfo"]:{
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedId+"date"]:serverTimestamp()
+        })
+      }
+    }catch(err){}
+  }
+  const handleSearch =async()=>{
+    const q=query(collection(db,"users"), where("displayName","==", username))
+    try{
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+      setUser(doc.data())
+      });
+    }catch(err){
+      setErr(true)
+    }
+  }
+  const handleKey= e=>{
+    e.code == "Enter" && handleSearch();
+  }
   return (
-    <div className='search d-flex justify-content-around  my-auto'>
-     
-        <input type='text' className='searchinput' placeholder='Tap to search'/>
-        <button className='my-auto border-0 btn btn-transparent'><BsSearch className='my-auto border-0 '/></button>
+    <div className='search justify-content-around  my-auto'>
+     <div className='d-flex'>
+      <input type='text' className='searchinput' placeholder='Tap to search' onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
+      <button className='my-auto border-0 btn btn-transparent' onClick={handleSearch}><BsSearch className='my-auto border-0 '/></button>
+      </div>
+  
+      {err && <span>User Not Found!</span>}
+      {user && <div className='chats' onClick={handleSelect}>
+        <img  src={user.photoURL} className='searchimg'/>
+      <div className='username'>
+        <span>{user.displayName}</span>
+      </div>
+      </div>}
     </div>
   )
-}
-
-export default search
+  }
+export default Search
